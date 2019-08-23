602abf7e6db361b85b59116cd41d115cb22258b7
Vimal Sheoran <vimalpsheoran@gmail.com>
2019-08-23 22:40:53 +0530

    Improved Implementation.

--- arrays/stock_buy_sell_problem.cpp
+++ arrays/stock_buy_sell_problem.cpp
@@ -5,19 +5,34 @@ using namespace std;
 int a[7] = {1, 5, 3, 8, 12, 11, 17};
 
 int main () {
-	int total_profit = 0;
-	int local_minima = -1, local_maxima = -1;
-	if (a[0] < a[1]) local_minima = a[0];
-	for (int i = 1; i < 7; i++) {
-		if (a[i] < a[i+1] && a[i] < a[i-1]) {
-			local_minima = a[i];
-		} else if (a[i] > a[i+1] && a[i] > a[i-1]) {
-			local_maxima = a[i];
-		}
-		if (local_minima != -1 and local_maxima != -1) {
-			total_profit += (local_maxima - local_minima);
-			local_minima = -1; local_maxima = -1;
+	int N;
+	cin >> N;
+	int a[N]; for (int i = 0; i < N; i++) cin >> a[i];
+
+	int min_index = -1;
+	bool min_found = false;
+	if (a[0] < a[1]) {
+		min_index = 0;
+		min_found = true;
+	}
+
+	for (int i = 1; i < N; i++) {
+		if (i == N-1) {
+			if (min_found == true) {
+				cout << "(" << min_index << " " << N-1 << ")" << " ";
+			}
+		} else {
+			if (min_found == true) {
+				if (a[i] > a[i+1] && a[i] > a[i-1]) {
+					cout << "(" << min_index << " " << i << ")" << " ";
+					min_found = false;
+				}
+			} else {
+				if (a[i] < a[i+1] && a[i] <= a[i-1]) {
+					min_index = i;
+					min_found = true;
+				}
+			}
 		}
 	}
-	printf("%d\n", total_profit);
 }
